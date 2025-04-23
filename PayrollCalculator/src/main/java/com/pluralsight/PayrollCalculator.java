43a109743fe33c0e56d5571238d42a9355326efb
zerkxes-a <annazerkle@gmail.com>
2025-04-23 11:42:41 -0400

    updated payroll calculator

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,23 +1,29 @@
 package com.pluralsight;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.Scanner;
+import java.io.*;
 import java.util.regex.Pattern;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 public class PayrollCalculator {
     public static void main(String[] args) {
         try {
+            Scanner userInput = new Scanner(System.in);
             //this is setting the start place(loading file)
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
-
+            System.out.print("Please enter the name of the file to read from: ");
+            String fileName = userInput.nextLine().trim();
+            System.out.print("Please enter the name of the payroll file you'd like to create: ");
+            String newFile = userInput.nextLine().trim();//wtire in new file
+            FileReader fileReader = new FileReader("src/main/resources/" + fileName);
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + newFile);
             //buffered reader added to load file incrementally
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             String input;
+
 
             //disregard the first line of code from while loop, read first + discard
             bufferedReader.readLine();
-
+            bufferedWriter.write("id | name | gross pay \n");
             //take each line and split into the 4 different parts, go over the entire block of text from source material
             while ((input = bufferedReader.readLine()) != null) {
                 //splits here
@@ -30,12 +36,23 @@ public class PayrollCalculator {
                 //now you have a new employee from the file(database can be used as well)
                 Employee employee = new Employee(employeeID, name, hoursWorked, payRate);
 
+
                 //tokens array??? create new employee object
-                System.out.printf("Employee Name: %s Employee ID: %d Gross Pay: %.2f \n", employee.getName(), employee.getEmployeeID(), employee.getGrossPay());
+//                System.out.printf("Employee Name: %s Employee ID: %d Gross Pay: %.2f \n", employee.getName(), employee.getEmployeeID(), employee.getGrossPay());
+//file writer starts here, gotta keep it within the instance of each employee id to write into file
+                //setting up file writer
+
+                bufferedWriter.write(String.format("%d | %s | %.2f \n", employee.getEmployeeID(), employee.getName(), employee.getGrossPay()));
+
+
             }
             //close that shiz(UNIVERSITY) out
             bufferedReader.close();
+            bufferedWriter.close();
+
         } catch (Exception e) {
+            System.out.print("Error: file not available");
+
         }
 
     }
